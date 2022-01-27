a4902da4d3effd9c7c5e90906eadbd5437a64258
Harsh2220 <harshgajjar145@gmail.com>
2022-01-27 19:17:23 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
+import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider value="dark">
+    <ChakraProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
