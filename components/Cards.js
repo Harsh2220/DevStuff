4411fbb4d15b67eb1f80f78f5e377debe01c911f
Harsh2220 <harshgajjar145@gmail.com>
2022-02-01 13:16:34 +0530

    card animation

--- components/Cards.js
+++ components/Cards.js
@@ -9,57 +9,74 @@ import {
     Icon,
     Link
 } from '@chakra-ui/react';
+import { AnimatePresence, motion } from 'framer-motion';
 import { FiExternalLink } from 'react-icons/fi';
+
+const MotionCenter = motion(Center);
+
+const Variants = {
+    hidden: {
+        opacity: 0
+    },
+    visible: {
+        opacity: 1,
+        transition:{
+            duration: 2
+        }
+    }
+}
 
 export default function Cards({ name, link, detail, url }) {
     return (
-        <Center py={12} m={5}>
-            <Box
-                role={'group'}
-                p={6}
-                maxW={'330px'}
-                minH={'500px'}
-                w={'full'}
-                rounded={'lg'}
-                pos={'relative'}
-                border={'1px'}
-                borderColor={'white'}
-                zIndex={1}>
+        <AnimatePresence key='link'>
+            <MotionCenter py={12} m={5} initial="hidden" variants={Variants} whileInView="visible" exit="hidden">
                 <Box
-                    rounded={'xl'}
-                    mt={-12}
+                    role={'group'}
+                    p={6}
+                    maxW={'330px'}
+                    minH={'500px'}
+                    w={'full'}
+                    rounded={'lg'}
                     pos={'relative'}
-                    height={'250px'}
-                    bg={'white'}
-                >
-                    <Center height={'100%'}>
-                        <Image
-                            objectFit={'contain'}
-                            src={url}
-                            alt="Banner"
-                        />
-                    </Center>
-                </Box>
-                <Stack pt={10}>
-                    <Stack direction={'row'} alignItems={'center'} justifyContent={'space-between'}>
-                        <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500} color={'white'}>
-                            {name}
-                        </Heading>
-                        <Link href={link} isExternal>
-                            <Button pr={0} _focus={{ textDecoration: 'none' }} bg={'transparent'}>
-                                <Icon
-                                    mr="4"
-                                    fontSize="24"
-                                    as={FiExternalLink}
-                                />
-                            </Button>
-                        </Link>
+                    border={'1px'}
+                    borderColor={'white'}
+                    zIndex={1}>
+                    <Box
+                        rounded={'xl'}
+                        mt={-12}
+                        pos={'relative'}
+                        height={'250px'}
+                        bg={'white'}
+                    >
+                        <Center height={'100%'}>
+                            <Image
+                                objectFit={'contain'}
+                                src={url}
+                                alt="Banner"
+                            />
+                        </Center>
+                    </Box>
+                    <Stack pt={10}>
+                        <Stack direction={'row'} alignItems={'center'} justifyContent={'space-between'}>
+                            <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500} color={'white'}>
+                                {name}
+                            </Heading>
+                            <Link href={link} isExternal>
+                                <Button pr={0} _focus={{ textDecoration: 'none' }} bg={'transparent'}>
+                                    <Icon
+                                        mr="4"
+                                        fontSize="24"
+                                        as={FiExternalLink}
+                                    />
+                                </Button>
+                            </Link>
+                        </Stack>
+                        <Text fontWeight={400} fontSize={'lg'} color={'white'}>
+                            {detail}
+                        </Text>
                     </Stack>
-                    <Text fontWeight={400} fontSize={'lg'} color={'white'}>
-                        {detail}
-                    </Text>
-                </Stack>
-            </Box>
-        </Center>
+                </Box>
+            </MotionCenter>
+        </AnimatePresence>
     );
 }
