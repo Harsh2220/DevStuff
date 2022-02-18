653ea4955722168bb8cc9e36cf113e8802b60d69
Harsh2220 <harshgajjar145@gmail.com>
2022-02-18 11:19:25 +0530

    css added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/Layout'
+import '../styles/globals.css'
 import theme from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
